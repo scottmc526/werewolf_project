8c11b22499636e3fb7472e568add018c22c46f04
Scott Christensen <scottmc526@gmail.com>
2016-03-04 15:23:59 -0700

    database content prints to screen

--- ang_app/app/js/controllers.js
+++ ang_app/app/js/controllers.js
@@ -1,3 +1,10 @@
 app.controller("MainController", function($scope, $http){
    // no instance variables here....
 });
+
+
+app.controller('StoriesController', function($scope, $http, werewolfService){
+  werewolfService.getStories().then(function(payload){
+    $scope.story_collection = payload.data;
+  })
+})
