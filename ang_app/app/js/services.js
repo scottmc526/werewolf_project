262e552cf50fbab76c9e7aafa634ff371a1e0672
Scott Christensen <scottmc526@gmail.com>
2016-03-04 15:49:34 -0700

    heroku url

--- ang_app/app/js/services.js
+++ ang_app/app/js/services.js
@@ -2,7 +2,8 @@ app.service('werewolfService', function($http){
   var werewolfService = {};
 
   werewolfService.getStories = function(){
-    return $http.get('http://localhost:3000/stories')
+    // return $http.get('http://localhost:3000/stories')
+    return $http.get('https://morning-brushlands-65110.herokuapp.com/stories')
   }
   return werewolfService;
 })
