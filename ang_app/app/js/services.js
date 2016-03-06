5e4dff0f1ae24d217c934b6ed59befb7fea0517c
Scott Christensen <scottmc526@gmail.com>
2016-03-06 15:52:55 -0700

    removed localhost

--- ang_app/app/js/services.js
+++ ang_app/app/js/services.js
@@ -3,12 +3,10 @@ app.service('werewolfService', function($http){
 
   werewolfService.getStories = function(){
     return $http.get('https://morning-brushlands-65110.herokuapp.com/stories')
-    // return $http.get('http://localhost:3000/stories')
   },
 
   werewolfService.postStory = function(story){
     return $http.post('https://morning-brushlands-65110.herokuapp.com/stories', story, {method:'jsonp'})
-    // return $http.post('http://localhost:3000/stories', story, {method:'jsonp'})
   }
   return werewolfService;
 })
