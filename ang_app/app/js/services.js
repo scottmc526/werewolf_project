1e70ac9db6b82a610a6984151976d9ac38f526b0
Patrick Dullano <PWDullano@gmail.com>
2016-03-06 17:54:54 -0700

    uncommented url in services

--- ang_app/app/js/services.js
+++ ang_app/app/js/services.js
@@ -2,13 +2,13 @@ app.service('werewolfService', function($http){
   var werewolfService = {};
 
   werewolfService.getStories = function(){
-    // return $http.get('https://morning-brushlands-65110.herokuapp.com/stories')
-    return $http.get('http://localhost:3000/stories')
+    return $http.get('https://morning-brushlands-65110.herokuapp.com/stories')
+    // return $http.get('http://localhost:3000/stories')
   },
 
   werewolfService.postStory = function(story){
-    // return $http.post('https://morning-brushlands-65110.herokuapp.com/stories', story, {method:'jsonp'})
-    return $http.post('http://localhost:3000/stories', story, {method:'jsonp'})
+    return $http.post('https://morning-brushlands-65110.herokuapp.com/stories', story, {method:'jsonp'})
+    // return $http.post('http://localhost:3000/stories', story, {method:'jsonp'})
   }
   return werewolfService;
 })
